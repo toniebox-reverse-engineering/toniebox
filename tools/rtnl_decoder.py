0cda060fac9014ff7e539307584efbaa2adf2a2d
scilor <SciLor@users.noreply.github.com>
2020-01-26 21:44:21 +0100

    rtnl protobuf decoder v2

--- tools/rtnl_decoder.py
+++ tools/rtnl_decoder.py
@@ -6,12 +6,11 @@ import logging
 import argparse
 from pathlib import Path
 
+import struct
+
 logging.basicConfig(level=logging.DEBUG)
 
 FILE_MAGIC = bytearray([0x0D, 0x0A])
-BLOCK_MAGIC_START = bytearray([0x18, 0x01, 0x20])
-BLOCK_MAGIC_START_SKIP = bytearray([0x18, 0x04, 0x20])
-BLOCK_MAGIC_START_SKIP2 = bytearray([0x18, 0x03, 0x20])
 BLOCK_MAGIC_END = bytearray([0x45 ,0x38 ,0x1D ,0xE9 ,0x5D])
 
 parser = argparse.ArgumentParser()
@@ -38,12 +37,7 @@ cursor = len(FILE_MAGIC)
 class Data_Block:
     start: int
     end: int
-    data: bytearray
-    unknown_1: bytearray
-    protobuf_type: int
-    protobuf_length: int
-    data_block: bytearray
-    unknown_2: bytearray
+    data_raw: bytearray
 
     def __hexify(self, byteblock):
         return " ".join(["{:02x}".format(x) for x in byteblock])
@@ -52,62 +46,88 @@ class Data_Block:
         return "0x%0.2x" % integer
 
     def __repr__(self):
-        return "Data Block\n\tstart: %s\n\tend: %s\n\tunknown_1: %s\n\tprotobuf_type: %s\n\tprotobuf_length: %s\n\tblock: %s\n\tunknown_2: %s\n\tdata: %s\n\ttext: %s" \
-        % (self.__as_hex(self.start), self.__as_hex(self.end), self.__hexify(self.unknown_1), self.__as_hex(self.protobuf_type), self.protobuf_length, self.__hexify(self.data_block), self.__hexify(self.unknown_2), self.__hexify(self.data), self.data_block)
+        return "Data Block\n\tstart: %s\n\tend: %s\n\tdata_raw: %s\n\tdecode: %s" \
+        % (self.__as_hex(self.start), self.__as_hex(self.end), self.__hexify(self.data_raw), self.__decode_protobuf(self.data_raw))
 
-while not data[cursor:].startswith(BLOCK_MAGIC_START):
-    cursor += 1
-    if cursor == len(data)-1:
-        logging.error("No magic start found")
-        exit(1)
+    def __decode_protobuf(self, data):
+        result = ""
+        cursor = 0
+        while cursor < len(data)-1:
+            field_id = data[cursor]
+            cursor += 1
+            if field_id == 0x09: # 1
+                f1, cursor = self.__parse_fixed64(data, cursor)
+                result += "\n\t\t1: %i" % (f1)
+            elif field_id == 0x10: # 2
+                f2, cursor = self.__parse_variant(data, cursor)
+                result += "\n\t\t2: %i" % (f2)
+            elif field_id == 0x18: # 3
+                f3, cursor = self.__parse_variant(data, cursor)
+                result += "\n\t\t3: %i" % (f3)
+            elif field_id == 0x20: # 4
+                f4, cursor = self.__parse_variant(data, cursor)
+                result += "\n\t\t4: %i" % (f4)
+            elif field_id == 0x28: # 5
+                f5, cursor = self.__parse_variant(data, cursor)
+                result += "\n\t\t5: %i" % (f5)
+            elif field_id == 0x32: # 6
+                f6, cursor = self.__parse_string(data, cursor)
+                result += "\n\t\t6: %s" % (f6)
+            #elif field_id == 0x: # 7
+                #continue
+            elif field_id == 0x45: # 8
+                f8, cursor = self.__parse_fixed32(data, cursor)
+                result += "\n\t\t8: %i" % (f8)
+            #elif field_id == 0x: # 9
+                #f9, cursor = self.__parse_string(data, cursor)
+                #result += "\n\t\t9: %s" % (f9)
+        return result
 
-while data[cursor:].startswith(BLOCK_MAGIC_START):
-    start = cursor
-    cursor += len(BLOCK_MAGIC_START)
-
-    if data[cursor+2] == 0x3C or data[cursor+2] == 0x50:
-        unknown_1 = data[cursor:cursor+3]
-        cursor += 3
-    else:
-        unknown_1 = data[cursor:cursor+4]
-        cursor += 4
-
-    protobuf_type = data[cursor]
-    cursor += 1
-    if protobuf_type == 0x20:
-        protobuf_len = 32
-    else:
-        protobuf_len = data[cursor]
+    def __parse_variant(self, data, cursor):
+        return (int(data[cursor]), cursor+1)
+    def __parse_fixed32(self, data, cursor):
+        return (struct.unpack('<i', data[cursor:cursor+4]), cursor+4)
+    def __parse_fixed64(self, data, cursor):
+        return (struct.unpack('<q', data[cursor:cursor+8]), cursor+8)
+    def __parse_string(self, data, cursor):
+        length = data[cursor]
         cursor += 1
+        return (data[cursor:cursor+length].decode("utf-8", "ignore"), cursor+length)
+        
 
-    data_block = data[cursor:cursor+protobuf_len]
-    cursor += protobuf_len
-    if not data[cursor:].startswith(BLOCK_MAGIC_END):
-        logging.error("Magic block end not found")
-        data_block = Data_Block(start, 0, data[start:], unknown_1, protobuf_type, protobuf_len, data_block, bytearray([]))
-        print(data_block)
-        break
-    cursor += len(BLOCK_MAGIC_END)
-    unknown_2 = data[cursor:cursor+17]
-    cursor += 17
 
-    if unknown_1[0] == 0x01: # and protobuf_len == 0:
-        cursor +=13
-    elif unknown_1[0] == 0x08:
-        cursor += 9
 
+
+# First block
+start = cursor
+while cursor < len(data)-1:
+    if data[cursor:].startswith(BLOCK_MAGIC_END):
+        cursor += len(BLOCK_MAGIC_END)
+        if data[cursor] == 0x00 and data[cursor+1] == 0x00 and data[cursor+2] == 0x00:
+            end = cursor
+            cursor += 3
+            break
+        else:
+            logging.error("b1 no 3x 0x00 after magic %#x %#x %#x" % (data[cursor], data[cursor+1], data[cursor+2]))
+    cursor += 1
+
+data_block = Data_Block(start, end, data[start:end])
+print(data_block)
+
+while cursor < len(data)-1:
+    length = data[cursor]
+    cursor += 1
+    start = cursor
+    cursor += length
     end = cursor
-    data_block = Data_Block(start, end, data[start:end], unknown_1, protobuf_type, protobuf_len, data_block, unknown_2)
+    data_block = Data_Block(start, end, data[start:end])
     print(data_block)
-
-    while data[cursor:].startswith(BLOCK_MAGIC_START_SKIP): #skip block tbd
-        data_block = Data_Block(cursor, cursor+34, data[cursor:cursor+34], bytearray([]), 0, 0, bytearray([]), bytearray([]))
-        print(data_block)
-        cursor += 34
-    while data[cursor:].startswith(BLOCK_MAGIC_START_SKIP2): #skip block tbd
-        data_block = Data_Block(cursor, cursor+38, data[cursor:cursor+38], bytearray([]), 0, 0, bytearray([]), bytearray([]))
-        print(data_block)
-        cursor += 38
+    if cursor < len(data)-1:
+        if data[cursor] == 0x00 and data[cursor] == 0x00 and data[cursor] == 0x00:
+            cursor += 3
+        else:
+            logging.error("b2 no 3x 0x00 after magic %#x %#x %#x" % (data[cursor], data[cursor+1], data[cursor+2]))
+            break
 
 if cursor < len(data)-1:
     logging.error("Cursor not at the end (%i!=%i)" % (cursor, len(data)-1))
